5fc87d422172690d1a535927399206ec3092070e
Nguyen Anh Tuyen <tuyen@curama.jp>
2016-12-15 15:34:33 +0900

    check python versions

--- jp.py
+++ jp.py
@@ -3,16 +3,24 @@
 import json
 import random
 import sys  
-reload(sys)  
-sys.setdefaultencoding('utf-8')
+import platform
+current_version = platform.python_version()
+if current_version >= '3.4':
+    import importlib
+    importlib.reload(sys)
+elif current_version <= '3.3':
+    import imp
+    imp.reload(sys)
+else:
+    reload(sys)
 
 def get_vocabulary():
-	data = None
-	vocabulary = ''
-	with open('data.json') as data_file:
-		data = json.load(data_file)
-	if not data:
-		return "Error"
-	word = random.choice(data);
-	vocabulary = word['Japanese'] + "("+ word['Kana'] +"): " + word['Vietnamese']
-	return vocabulary
+    data = None
+    vocabulary = ''
+    with open('data.json') as data_file:
+        data = json.load(data_file)
+    if not data:
+        return "Error"
+    word = random.choice(data);
+    vocabulary = word['Japanese'] + "("+ word['Kana'] +"): " + word['Vietnamese']
+    return vocabulary
